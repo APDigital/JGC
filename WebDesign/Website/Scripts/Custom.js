72f63edc79befae20693a0454016dad4ab0e30a4
APDigital <aoifejo.pearson@googlemail.com>
2018-04-04 15:11:57 +0100

    JS Scroll

--- WebDesign/Website/Scripts/Custom.js
+++ WebDesign/Website/Scripts/Custom.js
@@ -10,8 +10,6 @@
     });
 };
 
-<<<<<<< HEAD
-
 $(document).ready(function () {
     $("a").on('click', function (event) {
         if (this.hash !== "") {
@@ -32,21 +30,3 @@ $(document).ready(function () {
         }
     })
 })
-=======
-smoothScrollingTo('#homePage');
-smoothScrollingTo('#aboutPage');
-smoothScrollingTo('#venuePage');
-smoothScrollingTo('#contactPage');
-
-function smoothScrollingTo(target) {
-    $('html,body').animate({ scrollTop: $(target).offset().‌​top }, 500);
-}
-
-$('a[href*=\\#]').on('click', function (event) {
-    event.preventDefault();
-    smoothScrollingTo(this.hash);
-});
-$(document).ready(function () {
-    smoothScrollingTo(location.hash);
-});
->>>>>>> 4182bfd490335db7142b4262f31687e726b3e0b5
