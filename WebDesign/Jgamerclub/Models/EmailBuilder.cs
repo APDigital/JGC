fad7f16ea5937544a79385884de985aafcd23b4b
APDigital <aoifejo.pearson@googlemail.com>
2019-02-12 21:22:12 +0000

    added custom email sender

--- WebDesign/Jgamerclub/Models/EmailBuilder.cs
+++ WebDesign/Jgamerclub/Models/EmailBuilder.cs
@@ -14,19 +14,30 @@ namespace Jgamerclub.Models
         }
         public void SendEmail()
         {
-            MailMessage mail = new MailMessage("aoifejo.pearson@googlemail.com", UsersEmail);
-            SmtpClient smtpClient = new SmtpClient();
-            smtpClient.Port = 25;
-            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-            smtpClient.UseDefaultCredentials = false;
-            smtpClient.Host = "smtp.gmail.com";
-            mail.Subject = Subject;
-            mail.Body = GetMessageBody();
-            smtpClient.Send(mail);
+
+
+            try
+            {
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+                MailMessage mail = new MailMessage();
+                mail.To.Add("YOUR EMAIL HERE");
+                mail.From = new MailAddress("JGC.ContactForm@gmail.com");
+                mail.Subject = Subject;
+                mail.Body = GetMessageBody();
+                smtpClient.EnableSsl = true;
+                smtpClient.Credentials = new System.Net.NetworkCredential("YOUR EMAIL HERE", "YOUR EMAIL PASSWORD HERE");
+
+                smtpClient.Send(mail);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
         }
         public string GetMessageBody()
         {
-            string body = string.Format("{0} {1} From {2}", MessageBody, Environment.NewLine, UsersName);
+            string body = string.Format("{0} {1}{1}From {2} {1}{3}", MessageBody, Environment.NewLine, UsersName, UsersEmail);
             return body;
 
         }
